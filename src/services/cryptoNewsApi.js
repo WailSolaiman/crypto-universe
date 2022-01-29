20d96190d694583d63b796a1401bfe9a9c69d421
Wail Solaiman <wailsolaiman@hotmail.com>
2022-01-29 14:38:59 +0100

    add new services to fetch data cryptos and cryptos-news from API using redux-toolkit

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '2165a6d469msh5e8c16f84ee6c79p1ad00cjsn57b2695d06fe',
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders })
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) =>
+                createRequest(
+                    `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+                ),
+        }),
+    }),
+})
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi
