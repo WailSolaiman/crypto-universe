147f639d59b69da9795ac9d0242913e9298795b3
Wail Solaiman <wailsolaiman@hotmail.com>
2022-01-29 21:30:25 +0100

    add process.env vatiable to api headers

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const cryptoNewsHeaders = {
     'x-bingapis-sdk': 'true',
     'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-    'x-rapidapi-key': '2165a6d469msh5e8c16f84ee6c79p1ad00cjsn57b2695d06fe',
+    'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
 }
 
 const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
